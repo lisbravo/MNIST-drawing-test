593f449cff40e4c068eca5c8c6e05e6475cde7d9
lisbravo <lisandro.bravo@outlook.com>
2019-12-06 12:02:42 -0300

    working

--- MNIST drawing test.py
+++ MNIST drawing test.py
@@ -7,11 +7,13 @@ import cv2
 import matplotlib.pyplot as plt
 import PIL.Image, PIL.ImageTk
 import tensorflow as tf
+from threading import Timer
 
 
 class main:
     point = namedtuple('Point', ['x', 'y'])
     point_list = []
+    RECTANGLE_MARGIN = 5
     
     #angle detection variables and CONSTANTS
     current_angle = None
@@ -20,9 +22,9 @@ class main:
     ANGLE_MAX_LIMIT = 220
     ACCUTE_ANGLE_TRIGGER = 3  # number of deteccted angles in the windows to trigger an "accute angle detected" routine
     number_of_accute_angles = 0
-    accute_angle_cleared = False # indicates that the first, ascending line was detected al removed 
+    accute_angle_cleared = False # indicates that the first, ascending line was detected and removed 
 
-    #aspect ratio variables and constant
+    #aspect ratio variables and constants
     margin_left = 0
     margin_rigth = 0
     margin_top = 0
@@ -35,8 +37,21 @@ class main:
     scaled_image = None
     roi_image = None
 
+    #for inference
+    TIME_TO_INFERENCE = 2 #this is the time to wait between a start condition (accute angle detected) and inference 
+    WATCHDOG_TIME = 4 # To avoid blocking in the case that current drawing is too noisy  or gable
+    PREDCITED_LABEL = 'Predicted #'
+    PREDICTION_THRESHOLD = 0.7
+    results=None
+    inference_timer=None
+    watchdog_timer=None
+    # Graphic related 
+    progress_bar_array = []
+    progress_bar_val = []
+    predictions_labels_array = []
+    predictions_labels_var = []
 
-
+    
     def __init__(self,master):
         self.master = master
         self.color_fg = 'black'
@@ -48,7 +63,6 @@ class main:
         self.check_live = BooleanVar()
         self.check_live.set(True) 
         self.highlight_or_clear = IntVar(value=2)
-         
         self.drawWidgets()
         self.c.bind('<B1-Motion>',self.paint)#drwaing the line 
         self.c.bind('<ButtonRelease-1>',self.reset)
@@ -56,9 +70,8 @@ class main:
         
     def init_tensorflow(self):
         # from: https://www.tensorflow.org/lite/guide/inference#load_and_run_a_model_in_python
-        
         # Load TFLite model and allocate tensors.
-        self.interpreter = tf.lite.Interpreter(model_path='./MNIST_basic.tflite')
+        self.interpreter = tf.lite.Interpreter(model_path='./MNIST_cnn_basic.tflite') 
         self.interpreter.allocate_tensors()
 
         # Get input and output tensors.
@@ -67,51 +80,64 @@ class main:
 
 
     def paint(self,e):
-        RECTANGLE_MARGIN = 5
-
         self.point_list.append(self.point(x = e.x,y = e.y))
         
+        # first accute angle detection 
         if len(self.point_list) > self.WINDOW_FOR_ANGLE_DETECTION:
             mid_point = -1 - (round(self.WINDOW_FOR_ANGLE_DETECTION / 2))
             start_point = -1 - self.WINDOW_FOR_ANGLE_DETECTION
             self.current_angle = self.get_angle(self.point_list[start_point],self.point_list[mid_point],self.point_list[-1])
         
         if self.old_x and self.old_y:
-            
             self.c.create_line(self.point_list[-2].x ,self.point_list[-2].y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
 
+            #first line with accute angle removal
             if self.current_angle is not None \
                  and  (self.current_angle >= self.ANGLE_MAX_LIMIT \
                        or self.current_angle <= self.ANGLE_MIN_LIMIT):
-                
                 self.number_of_accute_angles +=1
+                
                 if self.number_of_accute_angles >= self.ACCUTE_ANGLE_TRIGGER \
                     and self.accute_angle_cleared == False:
                     self.clear_path( (len(self.point_list) ) )
                     self.number_of_accute_angles=0
                     self.accute_angle_cleared=True
             
+            #setting the Region Of Interes     
             if self.margin_left   > e.x: self.margin_left   = e.x 
             if self.margin_rigth  < e.x: self.margin_rigth  = e.x 
             if self.margin_top    > e.y: self.margin_top    = e.y 
             if self.margin_bottom < e.y: self.margin_bottom = e.y
 
+            #ratio is used to trigger the inference system, basically its a way to emulate a "pen up" event
             x_length,y_length,ratio = self.get_ratio(self.margin_left,self.margin_rigth,self.margin_top,self.margin_bottom)
             
+            #at this point we have a valid input
             if (ratio >= self.RATIO_TRIGGER or self.minimun_ratio_trigered == True) \
                 and self.accute_angle_cleared:
                 rectangle_color = "yellow"
                 self.minimun_ratio_trigered=True
                 self.process_roi(y_length,x_length,self.margin_top,self.margin_left)
+                # start inference timer and cancel the watchdog
+                if (self.inference_timer is None) or \
+                        (not self.inference_timer.is_alive()):
+                    self.inference_timer = Timer(self.TIME_TO_INFERENCE, self.infer)
+                    self.inference_timer.start()
+                    self.watchdog_timer.cancel()
             else: 
                 rectangle_color = "black"
             
+            # drawing a rectangle to highlight the ROI
             if self.rectangle: self.c.delete(self.rectangle)
-            self.rectangle = self.c.create_rectangle(self.margin_left - RECTANGLE_MARGIN ,self.margin_bottom + RECTANGLE_MARGIN \
-                                                     ,self.margin_rigth + RECTANGLE_MARGIN ,self.margin_top - RECTANGLE_MARGIN \
+            self.rectangle = self.c.create_rectangle(self.margin_left - self.RECTANGLE_MARGIN ,self.margin_bottom + self.RECTANGLE_MARGIN \
+                                                     ,self.margin_rigth + self.RECTANGLE_MARGIN ,self.margin_top - self.RECTANGLE_MARGIN \
                                                      ,outline= rectangle_color)
         
         else:
+            # this is processed when it receives the firt point in a new "drawing"
+            # the watchdog is set to clear an invalid input after timeup
+            self.watchdog_timer = Timer(self.WATCHDOG_TIME,self.clear)
+            self.watchdog_timer.start()
             self.margin_left = e.x
             self.margin_rigth = e.x
             self.margin_top = e.y
@@ -119,23 +145,65 @@ class main:
 
         self.old_x = e.x
         self.old_y = e.y
-    
+
+    # refresh screen status with the new infered information
+    def update_inference_results(self,event=None):
+        prediction_string = None
+        #block drawing input
+        [self.c.unbind('<Motion>') if self.check_live.get() else self.c.unbind('<B1-Motion>') ]
+        
+        self.c.itemconfig(self.rectangle,outline='blue')
+        
+        if self.results[0][np.argmax(self.results[0])] >= self.PREDICTION_THRESHOLD:
+            prediction_string = 'Number:' + str(np.argmax(self.results[0]))
+        else: prediction_string ='Not Recognized'
+
+        for i in range(len(self.results[0])):
+            self.progress_bar_val[i].set(self.results[0][i])
+            self.predictions_labels_var[i].set(str(int(self.results[0][i] * 100)) + "%")
+
+        #locks any interaction, giving time to see the results
+        self.c.create_text(self.margin_rigth + (self.RECTANGLE_MARGIN * 15),self.margin_top + self.RECTANGLE_MARGIN \
+                            ,fill="darkblue",font="Times 20 italic bold", text= prediction_string)
+
+        wait_timer = Timer(2.0,self.clear) 
+        wait_timer.start() 
+
+    # inference routine
+    def infer(self,event=None):
+        #normalization and reshaping 
+        normalized_image = self.scaled_image.astype('float32') / 255 
+        normalized_image = tf.reshape(normalized_image, [-1,28,28,1])
+                
+        self.interpreter.set_tensor(self.input_details[0]['index'], normalized_image)
+        self.interpreter.invoke()
+        self.results = self.interpreter.get_tensor(self.output_details[0]['index'])   
+        
+        self.update_inference_results(self)
+        
+                
+    # this will process the valid Region Of Interest 
     def process_roi(self,y_length,x_length,y_start,x_start): 
         #print(y_length,x_length,y_start,x_start)
         MARGIN = 100
         HORIZONTAL_OFFSET = x_start  - int(MARGIN/2)
         VERTICAL_OFFSET = y_start  - int(MARGIN/2)
+        THRESHOLD = 64
         
+        # first it will create a new image with the ROI drawing, plus some margin for centering
         self.roi_image = np.zeros((y_length + MARGIN,x_length + MARGIN),dtype='uint8')
         
         for i in range(len(self.point_list)-2):
             line_init = (self.point_list[i].x - HORIZONTAL_OFFSET ,self.point_list[i].y - VERTICAL_OFFSET )
             line_end =(self.point_list[i + 1].x - HORIZONTAL_OFFSET,self.point_list[i + 1].y - VERTICAL_OFFSET)
-            cv2.line(self.roi_image, line_init, line_end, 255, 5)
-        
+            cv2.line(self.roi_image, line_init, line_end, 255, 8)
+        # then it resizes the imagee to the 28*28 format of the neural model
         self.scaled_image = cv2.resize(self.roi_image, dsize=(28, 28), interpolation=cv2.INTER_AREA)
+         # applies a simple Threshold operation, improves inference
+        self.scaled_image[self.scaled_image > THRESHOLD] = 255
         
         
+    #simple routine to get the vertical to horizontal ratio, used to trigger a detection mechanism     
     def get_ratio(self, _margin_left,_margin_rigth,_margin_top,_margin_bottom):
         lenght_horizontal = self.margin_rigth - self.margin_left
         lenght_vertical = self.margin_bottom - self.margin_top
@@ -143,6 +211,7 @@ class main:
         #print("Lenght X:", lenght_horizontal, "Lenght Y:", lenght_vertical, "Ratio:", horizontal_to_vertical_ratio)
         return lenght_horizontal, lenght_vertical, horizontal_to_vertical_ratio      
 
+    #eliminates the first line after an accute angle is detected
     def clear_path(self, point_list_index):
         self.c.delete(ALL)
         #highlight
@@ -164,30 +233,28 @@ class main:
         angle = round(angle)
         return angle
         
-
+    #reset the whole system
     def reset(self,e):    #reseting or cleaning the canvas 
         self.old_x = None
         self.old_y = None  
         self.current_angle=None  
         self.number_of_accute_angles=0
         self.accute_angle_cleared=False
-        self.minimun_ratio_trigered=False  
-
-    def changeW(self,e): #change Width of pen through slider
-        self.penwidth = e
-           
-
+        self.minimun_ratio_trigered=False
+ 
+    #Clear Image
     def clear(self,event=None):
         self.point_list.clear()
         self.c.delete(ALL)
         self.reset(self)
-
-    def plot_roi(self):
+        [pbar_val.set(0) for pbar_val in self.progress_bar_val]
+        [label_var.set(0) for label_var in self.predictions_labels_var]
+        self.change_drawing_mode()
+        self.scaled_image =None
+    
+    def plot_roi(self,event=None):
         plt.imshow(self.scaled_image)
         plt.show() 
-        _image=PIL.Image.fromarray(self.scaled_image)
-        img =  PIL.ImageTk.PhotoImage(_image)
-        self.mnist_output.configure(image=img)  
 
     def change_fg(self):  #changing the pen color
         self.color_fg=colorchooser.askcolor(color=self.color_fg)[1]
@@ -209,43 +276,44 @@ class main:
 
     def drawWidgets(self):
         self.controls = Frame(self.master,padx = 5,pady = 5)
-        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
-        self.slider = ttk.Scale(self.controls,from_= 1, to = 50,command=self.changeW,orient=HORIZONTAL)
-        self.slider.set(self.penwidth)
-        self.slider.grid(row=0,column=1,ipadx=30)
       
-        self.btnClear = ttk.Button(self.controls, text="CLEAR", command=self.clear).grid(row=1,column=0,padx=20, pady=0)
-        self.btn_plt_roi = ttk.Button(self.controls, text="Plot ROI", command=self.plot_roi).grid(row=1,column=1,padx=20, pady=0)
+        self.btnClear = ttk.Button(self.controls, text="CLEAR", command=self.clear).grid(row=0,column=0,padx=20, pady=0)
+        self.btn_plt_roi = ttk.Button(self.controls, text="Plot ROI", command=self.plot_roi).grid(row=0,column=1,padx=20, pady=0)
         
-        self.chkLive = Checkbutton(self.controls, text = "live drawing", command=self.change_drawing_mode, variable = self.check_live, height=5,width = 20).grid(row=1,column=2)
+        self.chkLive = Checkbutton(self.controls, text = "live drawing", command=self.change_drawing_mode, variable = self.check_live, height=5,width = 20).grid(row=0,column=2)
         
-        Label(self.controls, text='On Angle Action:',font=('arial 14')).grid(row=2,column=0)
+        Label(self.controls, text='On Angle Action:',font=('arial 10')).grid(row=1,column=0)
 
-        self.r1=Radiobutton(self.controls, text="highlight", variable=self.highlight_or_clear,value=1).grid(row=2,column=1)
-        self.r2=Radiobutton(self.controls, text="clear", variable=self.highlight_or_clear,value=2).grid(row=2,column=2)
+        self.r1=Radiobutton(self.controls, text="highlight", variable=self.highlight_or_clear,value=1).grid(row=1,column=1)
+        self.r2=Radiobutton(self.controls, text="clear", variable=self.highlight_or_clear,value=2).grid(row=1,column=2)
         
-        Label(self.controls, text='MNIST Output:',font=('arial 14')).grid(row=3,column=0)
-        self.mnist_output = Canvas(self.controls,width=28,height=28,bg=self.color_bg)
-        self.mnist_output.grid(row=3,column=1)
+        row_base = 2
         
-        row_base = 4
-        progress_bar_array = []
         for i in range(10):
-            label_text = 'Predicted #' + str(i) + ':'
+            label_text = self.PREDCITED_LABEL + str(i) + ':'
             Label(self.controls, text=label_text,font=('arial 10')).grid(row=row_base + i,column=0)
-            progress_bar_array.append(\
-                ttk.Progressbar(self.controls, orient = HORIZONTAL, length = 100, \
-                                mode = 'determinate', maximum=1)) 
-            progress_bar_array[i].grid(row=row_base + i,column=1)
-
+            
+            self.predictions_labels_var.append(StringVar())
+            self.predictions_labels_var[i].set("%0")
+            self.predictions_labels_array.append(\
+                Label(self.controls, textvariable=self.predictions_labels_var[i],font=('arial 10')).grid(row=row_base + i,column=1)
+                )
+            
+            self.progress_bar_val.append(DoubleVar())
+            self.progress_bar_array.append(\
+                    ttk.Progressbar(self.controls, orient = HORIZONTAL, length = 100, \
+                                    variable= self.progress_bar_val[i], mode = 'determinate', maximum=1)) 
+            self.progress_bar_array[i].grid(row=row_base + i,column=2)
+            
         self.controls.pack(side=LEFT)
-        
-        
         
         self.c = Canvas(self.master,width=640,height=480,bg=self.color_bg,)
         self.c.pack(fill=BOTH,expand=True)
         
+        #shorcuts for testing on the fly
         self.master.bind('c', self.clear)
+        self.master.bind('p', self.plot_roi)
+        self.master.bind('i', self.infer)
 
         self. change_drawing_mode()
 
@@ -256,3 +324,6 @@ if __name__ == '__main__':
     main(root)
     root.title('MNIST drawing test')
     root.mainloop()
+
+
+
--- Readme.md
+++ Readme.md
@@ -1,3 +1,51 @@
-Add project description
-more to come
+This small project is a simple and fast way to emulate a hand drawing digits in the air
+
+Its main purpose is to analyze a good approach to solve the air hand writing digit calculator, called writing_dynamic_gestures_calculator, part of my Hand Commander framework
+
+Its based on a sketchpad from https://github.com/abhishek305/ProgrammingKnowlegde-Tkinter-Series/blob/master/10th/Slider%20and%20color%20choos.py
+that was modified to allow continuous live drawing (no need to hold a mouse button)  simulating the constant "drawing" path of hand movements
+
+It also process the drawing to detect a valid digit and use a neural network to infer the current digit.
+
+The choosed neural network is from the MNIST Keras example:  https://keras.io/examples/mnist_cnn/
+This is the convolutional version which achieves an average 99.25% accuracy in a couple of minutes
+
+
+The main challenges in this scenario was to "emulate" the start and stop conditions normally used in live handwriting
+recognition, usually  pen down/up
+
+Also, when drawing a new digit, the first movement is an ascending diagonal path to get to the point when a new digit begins,
+since the camera will be registering your movements constantly, we need a way to detect such movement, this is a simple simulation on paper:
+
+![alt text](https://raw.githubusercontent.com//lisbravo/MNIST-drawing-test/on paper sim.jpg)
+
+Red highlights this first ascending path, Green the digit. 
+
+So, after some tests, I decided that currently the fastest way to remove these initial movement was to detect the first drawn acute change of path 
+as you can see highlighted in blue in the number one digit on previous image
+This has proven to be a pretty good solution as you can see:
+![alt text](https://raw.githubusercontent.com//lisbravo/MNIST-drawing-test/starting path detection 1.gif)
+
+And here you can see the code removing the initial move and also boxing the ROI (Region Of Interest) which is the base for further processing.   
+The rectangle's color change to yellow when the ratio of horizontal to vertical sizes is over a preset level, a basic filter to detect that a digit-like shape
+![alt text](https://raw.githubusercontent.com//lisbravo/MNIST-drawing-test/starting path detection 2.gif)
+
+Coincidently, this acute angle detection in combination with a proper ROI ratio trigger is also a good enough approach to detect the beginning of a digit drawing,
+so, next in order is to detect a drawing finished condition. This has proven to be difficult and there are many approaches to be tested, 
+but I decided for the simplest solution, to set a timer of x seconds from a start condition to allow the user to finish. After some tests,  seconds seems to give enough time
+In this moment, a timer is set 
+
+From this point on, the code will grab the ROI, scale and normalize it accordingly to the neural network inputs and will infer the drawing.
+There is threshold applied to the inference results to guarantee a good recognition
+
+Also is worth to mention that a watchdog timer is started after the first movement is detected, that will trigger if no valid input is detected
+
+Even do everything is perfectible, end result is pretty satisfactory:
+![alt text](https://raw.githubusercontent.com//lisbravo/MNIST-drawing-test/end result 1.gif)
+![alt text](https://raw.githubusercontent.com//lisbravo/MNIST-drawing-test/end result 2.gif)
+
+
+
+
+
 
